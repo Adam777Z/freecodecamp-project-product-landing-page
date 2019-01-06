781184b3a7f20c9bc354907595475f8ffbbc40a1
Adam777 <Adam777-5867@outlook.com>
2019-01-06 16:04:53 +0000

    Fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const projectName = 'product-landing-page';
 localStorage.setItem('example_project', 'Product Landing Page');
 
-// $('body').scrollspy({
-// 	target: '#nav-bar'
+// $(document).ready(function() {
+// 	$('body').scrollspy({
+// 		target: '#nav-bar'
+// 	});
 // });
